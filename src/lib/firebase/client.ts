2c48577a0e92e5625edd65ba72c3fa69b7f673df
sysadmin-prithvi <info@prithvitechnologies.com>
2026-01-19 00:23:11 +0100

    fix

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+
 function getFirebaseClientApp() {
   const existing = getApps()[0];
   if (existing) return existing;
